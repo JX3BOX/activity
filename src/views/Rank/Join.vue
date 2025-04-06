79f475667e6d69fc27f3e91a752d0c20de226e00
xuan <litthy@qq.com>
2025-04-06 11:04:06 +0800

    fix: confirm

--- src/views/Rank/Join.vue
+++ src/views/Rank/Join.vue
@@ -12,17 +12,32 @@
                     <div class="m-join m-join-team">
                         <h1 class="m-join-title">报名入口</h1>
                         <div class="m-join-notice" v-html="notice"></div>
-                        <el-form class="m-join-form" ref="form" :model="form" label-width="80px" v-if="!loading && !status">
+                        <el-form
+                            class="m-join-form"
+                            ref="form"
+                            :model="form"
+                            label-width="80px"
+                            v-if="!loading && !status"
+                        >
                             <el-form-item label="报名活动">
                                 <el-select v-model="form.event_id" placeholder="请选择活动" no-data-text="暂无活动">
-                                    <el-option v-for="event in events" :key="event.ID" :label="event.name" :value="event.ID">
+                                    <el-option
+                                        v-for="event in events"
+                                        :key="event.ID"
+                                        :label="event.name"
+                                        :value="event.ID"
+                                    >
                                     </el-option>
                                 </el-select>
                             </el-form-item>
                             <el-form-item label="选择团队">
                                 <el-select v-model="form.team_id" placeholder="请选择团队" @change="updateTeam">
-                                    <el-option v-for="team in teams" :key="team.ID" :label="team.name" :value="team.ID"><span class="m-join-team-item"><b class="u-team-name">{{
-                                                team.name }}</b><span class="u-team-id">(ID:{{ team.ID }})</span><span class="u-team-server">{{ team.server }}</span></span>
+                                    <el-option v-for="team in teams" :key="team.ID" :label="team.name" :value="team.ID"
+                                        ><span class="m-join-team-item"
+                                            ><b class="u-team-name">{{ team.name }}</b
+                                            ><span class="u-team-id">(ID:{{ team.ID }})</span
+                                            ><span class="u-team-server">{{ team.server }}</span></span
+                                        >
                                     </el-option>
                                 </el-select>
                                 <div class="u-tip" v-if="!teams || !teams.length">
@@ -30,11 +45,18 @@
                                 </div>
                             </el-form-item>
                             <el-form-item label="参赛宣言">
-                                <el-input v-model="form.slogan" placeholder="为您的团队打CALL,将显示在游戏内" :maxlength="20" show-word-limit></el-input>
+                                <el-input
+                                    v-model="form.slogan"
+                                    placeholder="为您的团队打CALL,将显示在游戏内"
+                                    :maxlength="20"
+                                    show-word-limit
+                                ></el-input>
                             </el-form-item>
                             <div class="u-btns">
                                 <!--                                <div class="u-warning" v-show="status"><i class="el-icon-warning-outline"></i>当前活动你名下的【{{joined_team_name}}】已报名，无需重复报名。</div>-->
-                                <el-button class="u-btn" type="primary" @click="submit" :disabled="!ready">报名</el-button>
+                                <el-button class="u-btn" type="primary" @click="submit" :disabled="!ready"
+                                    >报名</el-button
+                                >
                             </div>
                         </el-form>
                         <div class="m-join m-join-done" v-if="status">
@@ -53,7 +75,9 @@
                     </div>
 
                     <div class="u-footer">
-                        <a href="/notice/32280" target="_blank"><i class="el-icon-info"></i> <b>点击查看百强活动细则</b></a>
+                        <a href="/notice/32280" target="_blank"
+                            ><i class="el-icon-info"></i> <b>点击查看百强活动细则</b></a
+                        >
                     </div>
                 </div>
                 <div class="m-rank-content m-rank-null" v-else>
@@ -114,6 +138,7 @@ export default {
             },
             processing: false, //按钮提交锁定
             notice: "",
+            confirm: "",
 
             // 用户信息
             profile: null,
@@ -157,7 +182,7 @@ export default {
                 return;
             }
 
-            this.$alert("报名后资料将不可再更改，更多咨询请联系认证团长Q群【1048059072】", "消息", {
+            this.$alert(this.confirm, "消息", {
                 confirmButtonText: "确定",
                 callback: (action) => {
                     if (action == "confirm") {
@@ -206,7 +231,7 @@ export default {
             });
         },
         checkJoin: function () {
-            if (!this.form.event_id) return
+            if (!this.form.event_id) return;
             this.loading = true;
             hasJoined(this.form.event_id).then((res) => {
                 this.result = res.data.data;
@@ -227,6 +252,9 @@ export default {
         loadNotice: function () {
             getBreadcrumb("rank-join").then((data) => {
                 this.notice = data;
+            });
+            getBreadcrumb("rank-join-confirm").then((data) => {
+                this.confirm = data;
             });
         },
         init: function () {
@@ -257,7 +285,7 @@ export default {
         this.isLogin && this.init();
     },
     components: {
-        BindWxMp
+        BindWxMp,
     },
 };
 </script>
