1405d0c7a0f896b09481ee6da5c5551a2ce5f463
Kaviilee <jiaweilee95@126.com>
2025-07-28 18:33:46 +0800

    feat: 情缘杯 - 活动首页 #5

--- src/views/Lover/Info.vue
+++ src/views/Lover/Info.vue
@@ -1,36 +1,43 @@
 <template>
-    <div class="m-lover-info m-lover-page wp">
+    <div class="m-lover-info wp">
         <div class="u-page-title">
-            <img class="u-img" :src="`${__imgRoot}info-title.png`" />
+            <img class="u-title-img" :src="`${__imgRoot}info-title.png`" />
         </div>
-        <div class="m-lover-box m-info-box">
-            <h2>情缘杯</h2>
-            <div v-html="describe"></div>
+        <div class="m-desc-box">
+            <div class="m-info-box">
+                <div class="u-text" v-html="describe"></div>
+            </div>
+            <!-- <h2>情缘杯</h2> -->
+            <img class="u-top-left" :src="`${__imgRoot}fengye1.svg`" alt="" />
+            <img class="u-bottom-right" :src="`${__imgRoot}fengye2.svg`" alt="" />
         </div>
         <div class="m-lover-process">
-            <div class="m-step" v-for="(item, i) in process" :key="i">
-                <div class="u-icon">
-                    <img class="u-img" :src="`${__imgRoot}step${i + 1}.svg`" />
+            <img class="u-step-img" :src="`${__imgRoot}info-step.png`" alt="">
+            <div class="m-steps">
+                <div class="m-step" v-for="(item, i) in process" :key="i">
+                    <div class="u-icon">
+                        <img class="u-img" :src="`${__imgRoot}step${i + 1}.svg`" />
+                    </div>
+                    <span class="u-dot"><i></i></span>
+                    <span class="u-name">{{ item.text }}</span>
+                    <span class="u-time">{{ item.time }}</span>
+                    <!-- <span class="u-desc">{{ item.desc }}</span> -->
                 </div>
-                <span class="u-dot"><i></i></span>
-                <span class="u-name">{{ item.text }}</span>
-                <span class="u-time">{{ item.time }}</span>
-                <!-- <span class="u-desc">{{ item.desc }}</span> -->
             </div>
         </div>
-        <div class="m-lover-box">
-            <h3>奖品</h3>
-            <div v-html="gifts"></div>
+        <div class="m-lover-gift">
+            <img class="u-gift-img" :src="`${__imgRoot}info-award.png`" alt="">
+            <div class="m-gift-content" v-html="gifts"></div>
         </div>
-        <div class="m-lover-box">
-            <h3>赛事规则</h3>
-            <div v-html="rule"></div>
+        <div class="m-lover-rule">
+            <img class="u-rule-img" :src="`${__imgRoot}info-rule.png`" alt="">
+            <div class="m-rule-content" v-html="rule"></div>
         </div>
     </div>
 </template>
 
 <script>
-import { getLoverEvent } from "@/service/rank/lover";
+import dayjs from "dayjs";
 export default {
     name: "LoverInfo",
     inject: ["__imgRoot"],
