0cce9c5f5bebda191192667dcd4a99ed48bc9aab
Kaviilee <jiaweilee95@126.com>
2025-08-03 17:04:03 +0800

    fix: 修复资源路径，更新背景图和按钮图标引用

--- src/views/Lover/Info.vue
+++ src/views/Lover/Info.vue
@@ -9,7 +9,7 @@
             </div>
             <img class="u-top-left" :src="`${__imgRoot}fengye1.svg`" alt="" />
             <img class="u-bottom-right" :src="`${__imgRoot}fengye2.svg`" alt="" />
-            <router-link :to="{name: 'join'}"><img class="u-join-btn" src="../../../tmp/to-join.png" alt=""></router-link>
+            <router-link :to="{name: 'join'}"><img class="u-join-btn" :src="`${__imgRoot}to-join.png`" alt=""></router-link>
         </div>
         <div class="m-lover-process">
             <img class="u-step-img" :src="`${__imgRoot}info-step.png`" alt="" />
