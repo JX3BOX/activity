56e9e2e839ff4bce01bbb57d6b6341d71c968301
x3zvawq <x3zvawq@gmail.com>
2025-08-09 14:51:55 +0800

    feat(lover): 赛程页完成

--- src/components/rank/lover/battle_detail.vue
+++ src/components/rank/lover/battle_detail.vue
@@ -1,59 +1,55 @@
 <template>
-    <el-dialog :visible="visible" custom-class="m-lover-battle-detail__dialog" :show-close="false" append-to-body>
+    <el-dialog
+        :visible="visible"
+        custom-class="m-lover-battle-detail__dialog"
+        :show-close="false"
+        append-to-body
+        :style="containerStyle"
+    >
         <template #title>
-            <div class="m-detail-header" v-if="renderProcess">
-                <div
-                    class="m-team-info"
-                    v-for="(team, index) in [renderProcess.team1_record, renderProcess.team2_record]"
-                    :class="{ 'is-reverse': index === 0 }"
-                    :key="index"
-                >
-                    <img class="u-team-icon" :src="team.images[0]" alt="" />
-                    <div class="m-team-section">
-                        <div class="m-team-name">
-                            <span class="u-name">{{ team.team_name }}</span>
-                            <span class="u-live">进入直播间</span>
-                        </div>
-                        <div class="m-team-mates">
-                            <span v-for="(user, index) in team.teammates_info" :key="index">{{
-                                user.display_name
-                            }}</span>
-                        </div>
-                    </div>
-                    <img
-                        class="u-card"
-                        v-if="process[`team${index + 1}_card`]"
-                        :src="cardLink(process[`team${index + 1}_card`])"
-                    />
-                </div>
-                <div class="u-center-vs">
-                    <img class="u-vs" :src="`${cdnLink}design/event/lover/vs.png`" alt="" />
-                    <img class="u-round" v-if="process" :src="headImage[process.round - 1]" alt="" />
-                </div>
-            </div>
             <el-button class="el-dialog__headerbtn" type="text" @click="visible = false">
                 <img src="@/assets/img/rank/lover/close.svg" svg-inline alt="" />
             </el-button>
-            <div class="u-bottom-border"></div>
         </template>
-        <div class="m-detail-pz">
-            <div class="m-detail-team"></div>
+        <div class="m-battle-detail" v-if="renderProcess">
+            <battle-detail-team
+                class="is-left"
+                :team="renderProcess.team1_record"
+                :card="cardInfo(renderProcess.team1_card)"
+            ></battle-detail-team>
+            <div class="m-middle">
+                <img class="u-logo" :src="`${cdnLink}design/event/lover/battle_lover_logo.webp`" alt="" />
+                <img class="u-vs" :src="`${cdnLink}design/event/lover/vs.png`" alt="" />
+                <img class="u-round" v-if="renderProcess" :src="headImage[renderProcess.round - 1]" alt="" />
+                <span class="u-time">{{ renderProcess.start_time }} </span>
+            </div>
+            <battle-detail-team
+                :team="renderProcess.team2_record"
+                :card="cardInfo(renderProcess.team2_card)"
+            ></battle-detail-team>
         </div>
     </el-dialog>
 </template>
 
 <script>
+import BattleDetailTeam from "@/components/rank/lover/battle_detail_team.vue";
 import { cloneDeep } from "lodash";
+import { getEventCardList } from "@/service/rank/lover";
+
 export default {
     name: "LoverBattleDetail",
-    components: {},
+    components: { BattleDetailTeam },
     props: {},
     inject: ["cdnLink", "headImage"],
     data: () => ({
         visible: false,
         process: null,
+        cards: [],
     }),
     computed: {
+        currentEvent() {
+            return this.$store.state.event;
+        },
         renderProcess() {
             if (!this.process) return null;
             const result = cloneDeep(this.process);
@@ -67,50 +63,51 @@ export default {
             };
             return result;
         },
+        containerStyle() {
+            return {
+                "--bg-image": `url(${this.cdnLink}design/event/lover/battle_detail_bg.webp)`,
+            };
+        },
     },
     methods: {
-        cardLink(id) {
-            return `${this.cdnLink}design/event/lover/q${id}_small.png`;
+        loadCards() {
+            getEventCardList({
+                _no_page: 1,
+                event_id: this.currentEvent.id,
+            }).then((res) => {
+                this.cards = res.data.data.list || [];
+            });
         },
         open(process) {
             this.visible = true;
             this.process = process;
             console.log("open process", this.process);
         },
+        cardInfo(id) {
+            return this.cards.find((item) => item.id === id) || {};
+        }
+    },
+    mounted() {
+        this.loadCards();
     },
 };
 </script>
 
 <style lang="less">
 .m-lover-battle-detail__dialog {
-    .w(1314px);
+    .w(869px);
     box-sizing: border-box;
-    border: 2px solid #f3c768ff;
+    border: 1px solid #f3c768ff;
     border-top: 10px solid #f3c768ff;
     background: rgba(105, 63, 51, 1);
 
     .el-dialog__header {
         .pr;
-        padding: 10px 62px 0 62px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        gap: 12px;
-
-        .u-bottom-border {
-            .size(800px, 2px);
-            background: linear-gradient(
-                90deg,
-                rgba(242, 199, 104, 0) 0%,
-                rgba(243, 199, 104, 1) 25.88%,
-                rgba(243, 199, 104, 0.96) 74.33%,
-                rgba(243, 199, 104, 0) 100%
-            );
-        }
+        padding: 0;
     }
     .el-dialog__headerbtn {
         .pa;
-        .rt(12px);
+        .rt(0px);
         .size(40px);
         padding: 0;
         display: flex;
@@ -123,105 +120,41 @@ export default {
             line-height: 0;
         }
     }
-    .m-detail-header {
-        box-sizing: border-box;
-        background: linear-gradient(90deg, rgba(0, 0, 0, 0) 0%, rgba(74, 40, 30, 1) 50%, rgba(0, 0, 0, 0) 100%);
-        .size(100%, 92px);
+    .el-dialog__body {
+        padding: 40px 0;
+        background-image: var(--bg-image);
+        background-size: cover;
+    }
+    .m-battle-detail {
+        opacity: 1;
+        background: rgba(77, 42, 33, 0.5);
+        display: flex;
         justify-content: center;
-        align-items: center;
-        gap: 20px;
-        .pr;
-        display: flex;
-        justify-content: space-between;
-
-        .u-center-vs {
-            .pa;
-            top: 14px;
-            left: 553px;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            .size(80px, 64px);
-        }
-
-        .u-vs {
-            .size(48px, 40px);
-        }
-
-        .u-round {
-            max-height: 36px;
-        }
+        padding: 24px 70px;
+        gap: 36px;
     }
-    .m-team-info {
+
+    .m-middle {
         display: flex;
         align-items: center;
-        width: 534px;
-        height: 68px;
-        gap: 12px;
-
-        .u-team-icon {
-            .size(64px);
-        }
-
-        .u-card {
-            .size(40px, 64px);
-        }
-
-        &.is-reverse {
-            &,
-            .m-team-name,
-            .m-team-mates {
-                flex-direction: row-reverse;
-            }
-        }
-    }
-    .m-team-section {
-        display: flex;
         flex-direction: column;
-        flex-grow: 1;
-    }
-    .m-team-name {
-        display: flex;
-        align-items: center;
-        gap: 12px;
 
-        .u-name {
-            .fz(28px, 40.54px);
-            font-weight: 700;
-            color: white;
+        .u-logo {
+            .size(97px, 70px);
+            .mb(20px);
         }
-        .u-live {
-            height: 22px;
-            box-sizing: border-box;
-            .r(4px);
-            border: 1px solid rgba(243, 199, 104, 1);
-            color: rgba(243, 199, 104, 1);
-            padding: 2px 4px;
-
+        .u-vs {
+            .size(65px, 54px);
+            .mb(8px);
+        }
+        .u-round {
+            .w(80px);
+            .mb(8px);
+        }
+        .u-time {
             .fz(12px, 17.38px);
-            font-weight: 700;
-
-            &.is-no-start {
-                color: rgba(166, 166, 166, 1);
-            }
-
-            &:not(.is-no-start) {
-                cursor: pointer;
-                &:hover {
-                    background: white;
-                }
-            }
+            color: rgba(255, 255, 255, 0.5);
         }
-    }
-    .m-team-mates {
-        display: flex;
-        align-items: center;
-        gap: 12px;
-
-        .fz(18px, 26.06px);
-        font-weight: 400;
-        color: rgba(255, 232, 204, 1);
     }
 }
 </style>
