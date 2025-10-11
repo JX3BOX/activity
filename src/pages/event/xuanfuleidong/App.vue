5f004511b1c3fdd6d62b425d8bfa6968ddd7820e
litthy <litthy@qq.com>
2025-10-11 17:43:16 +0800

    feat: xuanfuleidong

---
+++ src/pages/event/xuanfuleidong/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- 专题页 -->
+    <div class="p-event" :class="'v-' + page_name">
+        <Header :overlayEnable="true"></Header>
+        <router-view></router-view>
+        <div class="p-event-footer">
+            <div class="wp">
+                <Footer></Footer>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { postStat } from "@jx3box/jx3box-common/js/stat";
+import { __cdn } from "@/utils/config";
+export default {
+    name: "App",
+    data: function () {
+        return {};
+    },
+    provide: {
+        __imgRoot: __cdn + "design/event/xuanfuleidong/",
+    },
+    computed: {
+        page_name: function () {
+            return this.$route.name;
+        },
+    },
+    created: function () {
+        postStat("event", "xuanfuleidong");
+    },
+};
+</script>
---
+++ src/pages/event/xuanfuleidong/Index.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="m-index m-xuanfuleidong">
+        <div class="m-kv">
+            <img :src="`${__imgRoot}kv.jpg`" alt="玄府雷动" />
+            <div class="m-title">
+                <img :src="`${__imgRoot}title.png`" alt="玩法创意大赛" />
+                <img :src="`${__imgRoot}no${index}.png`" class="u-no" :alt="`第${index}届`" />
+                <p class="u-desc">
+                    侠影开新境，巧思妙笔神。至微至玄、不可察之空隙，亦能震荡躁动，声势若雷鸣，名曰玄府雷动。<br />
+                    侠士是江湖最微小的构成点，是否也心有丘壑，意蕴磅礴，遂于一瞬，巧思如泉涌，欲为江湖开辟新局、创作出新鲜的玩法呢？<br />
+                    快拿起手中的生花妙笔，祝侠士奇思异想如潮生，同品江湖新章！
+                    <span class="u-desc-shadow">
+                        侠影开新境，巧思妙笔神。至微至玄、不可察之空隙，亦能震荡躁动，声势若雷鸣，名曰玄府雷动。<br />
+                        侠士是江湖最微小的构成点，是否也心有丘壑，意蕴磅礴，遂于一瞬，巧思如泉涌，欲为江湖开辟新局、创作出新鲜的玩法呢？<br />
+                        快拿起手中的生花妙笔，祝侠士奇思异想如潮生，同品江湖新章！
+                    </span>
+                </p>
+            </div>
+        </div>
+        <div class="m-tabs">
+            <div
+                v-for="(item, i) in tabs"
+                :key="i"
+                :class="[{ active: item.key == key }, [`u-tab-0${i + 1}`]]"
+                @click="handleTabClick(item.key)"
+            ></div>
+        </div>
+        <div class="m-content">
+            <div class="u-start"></div>
+            <img :src="`${__imgRoot}end.png`" class="u-line" alt="line" />
+            <div class="m-info">
+                
+            </div>
+            <img :src="`${__imgRoot}end.png`" class="u-line" alt="line" />
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "Index",
+    inject: ["__imgRoot"],
+    components: {},
+    data: function () {
+        return {
+            index: 1,
+            key: "rule",
+            tabs: [
+                { name: "活动介绍", key: "rule", active: true },
+                { name: "玩法投票", key: "prize", active: false },
+                { name: "获奖作品", key: "winner", active: false },
+            ],
+        };
+    },
+
+    computed: {},
+    watch: {},
+    methods: {
+        handleTabClick(key) {
+            this.key = key;
+        },
+    },
+};
+</script>
+
+<style lang="less">
+@import "~@/assets/css/event/xuanfuleidong/index.less";
+</style>
