f114cc10d8e6bfba262f37705cb6119694cd36ba
litthy <litthy@qq.com>
2025-10-27 11:04:02 +0800

    fix: hide

--- src/pages/event/xuanfuleidong/App.vue
+++ src/pages/event/xuanfuleidong/App.vue
@@ -3,17 +3,17 @@
     <div class="p-event" :class="[`v-${page_name}`, { isMiniProgram }]">
         <Header :overlayEnable="true"></Header>
         <router-view></router-view>
-        <div class="p-event-footer">
+        <!-- <div class="p-event-footer">
             <div class="wp">
                 <Footer></Footer>
             </div>
-        </div>
+        </div> -->
     </div>
 </template>
 
 <script>
 import { isMiniProgram } from "@jx3box/jx3box-common/js/utils";
-import { postStat } from "@jx3box/jx3box-common/js/stat";
+// import { postStat } from "@jx3box/jx3box-common/js/stat";
 import { __cdn } from "@/utils/config";
 export default {
     name: "App",
@@ -31,9 +31,9 @@ export default {
         },
     },
     created: function () {
-        postStat("event", "xuanfuleidong");
+        // postStat("event", "xuanfuleidong");
         // 小程序下设置viewport
-        if (this.isMiniProgram) {
+        if (isMiniProgram()) {
             let meta = document.createElement("meta");
             meta.setAttribute("name", "viewport");
             meta.setAttribute("content", "width=device-width,initial-scale=1.0, maximum-scale=1.0, user-scalable=no");
