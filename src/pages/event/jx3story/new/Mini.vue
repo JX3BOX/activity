a2c58ef9a9a8c5be40be792cfb605675277b6294
litthy <litthy@qq.com>
2025-11-25 12:45:29 +0800

    feat: mini

---
+++ src/pages/event/jx3story/new/Mini.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="p-jx3story-mini">
+        <img :src="`${cdn}2025/mini/index/logo.png`" />
+        <div class="m-menu">
+            <router-link class="u-menu-item" v-for="item in menu" :key="item.key" :to="item.link">
+                {{ item.name }}
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+import { __cdn } from "@/utils/config";
+export default {
+    name: "jx3storyMini",
+    inject: ["__imgRoot"],
+    data() {
+        return {
+            cdn: __cdn + "design/event/jx3story/",
+            menu: [
+                {
+                    name: "活动介绍",
+                    key: "info",
+                    link: { path: "/detail", query: { key: "info" } },
+                },
+                {
+                    name: "投票赏析",
+                    key: "vote",
+                    link: { path: "/detail", query: { key: "vote" } },
+                },
+                {
+                    name: "票数统计",
+                    key: "stats",
+                    link: { path: "/detail", query: { key: "stats" } },
+                },
+            ],
+        };
+    },
+    computed: {},
+};
+</script>
+
+<style lang="less">
+@import "~@/assets/css/event/jx3story/new/mini.less";
+</style>
--- src/pages/event/jx3story/router.js
+++ src/pages/event/jx3story/router.js
@@ -1,20 +1,23 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-
+import { isMiniProgram } from "@jx3box/jx3box-common/js/utils";
 Vue.use(VueRouter);
+const Mini = () => import("./new/Mini.vue");
+const Index = () => import("./new/Index.vue");
+const Detail = () => import("./new/Detail.vue");
 
 const router = new VueRouter({
     routes: [
         {
             path: "/",
             name: "Index",
-            component: () => import("./new/Index.vue"),
+            component: isMiniProgram() ? Mini : Index,
         },
         {
             path: "/detail",
             name: "Detail",
             meta: { key: "detail" },
-            component: () => import("./new/Detail.vue"),
+            component: Detail,
         },
     ],
 });
