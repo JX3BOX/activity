22bf6182f979d9549adea6d641c53dfd5033127f
xuan <litthy@qq.com>
2026-01-16 20:17:45 +0800

    chore: style

--- src/pages/event/jx3cxk/components/Item.vue
+++ src/pages/event/jx3cxk/components/Item.vue
@@ -2,16 +2,62 @@
     <div class="m-item-box">
         <div class="m-item">
             <div class="u-title">DEAD MAN</div>
+            <div class="u-author">练习生：djkjkm</div>
+            <div class="m-record">
+                <img class="u-needle" :src="`${imgRoot}web/item/needle.svg`" />
+                <div class="u-record">
+                    <img class="u-avatar" src="https://cdn.jx3box.com/upload/avatar/2021/3/5/9393123.jpg?" />
+                </div>
+            </div>
+            <el-slider v-model="slider" size="small" :show-tooltip="false" />
+            <div class="m-play">
+                <img class="u-icon" :src="`${imgRoot}web/item/left.svg`" />
+                <img class="u-icon u-play" :src="`${imgRoot}web/item/play.svg`" />
+                <img class="u-icon" :src="`${imgRoot}web/item/right.svg`" />
+            </div>
+            <div class="m-link">
+                <div class="u-like">
+                    <img class="u-icon" :src="`${imgRoot}web/item/like.svg?jx3cxk`" />
+                    982391
+                </div>
+                <a :href="``" target="_blank">查看原帖</a>
+            </div>
+        </div>
+        <div class="m-button">
+            <div class="u-call"><span>打call !</span></div>
+            <img class="u-icon" :src="`${imgRoot}web/item/emoji-1.svg`" />
+            <img class="u-icon u-hover" :src="`${imgRoot}web/item/emoji-2.svg`" />
+            <img class="u-icon u-active" :src="`${imgRoot}web/item/like.svg?jx3cxk`" />
         </div>
     </div>
 </template>
 <script>
 export default {
+    inject: ["__imgRoot"],
     data() {
-        return {};
+        return { imgRoot: this.__imgRoot, slider: 0 };
     },
 };
 </script>
 <style lang="less" scoped>
 @import "~@/assets/css/event/jx3cxk/item.less";
+</style>
+<style lang="less">
+.el-slider__button {
+    border: 2px solid rgba(65, 61, 82, 0.3);
+}
+.el-slider__runway {
+    height: 3px;
+    background-color: rgba(65, 61, 82, 0.3);
+}
+.el-slider__button-wrapper {
+    top: -17px;
+}
+.el-slider__bar {
+    height: 3px;
+    background-color: #fff;
+}
+.el-slider__button {
+    .size(12px);
+}
 </style>
--- src/pages/event/jx3cxk/components/Vote.vue
+++ src/pages/event/jx3cxk/components/Vote.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="m-content m-vote">
+    <div class="m-content m-vote wp">
         <vote-item></vote-item>
     </div>
 </template>
@@ -15,3 +15,14 @@ export default {
     },
 };
 </script>
+<style lang="less" scoped>
+.m-content.m-vote {
+    .r(48px);
+    box-sizing: border-box;
+    padding: 48px;
+    background: rgba(59, 46, 70, 0.5);
+    .flex;
+    flex-wrap: wrap;
+    gap: 28px;
+}
+</style>
