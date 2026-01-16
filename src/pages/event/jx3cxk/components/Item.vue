e896b54f5b9f98c2c3ffa1a18ffbaa9f8818d762
xuan <litthy@qq.com>
2026-01-17 02:05:53 +0800

    fix: item

--- src/pages/event/jx3cxk/components/Item.vue
+++ src/pages/event/jx3cxk/components/Item.vue
@@ -1,26 +1,43 @@
 <template>
-    <div class="m-item-box">
+    <div class="m-item-box" v-if="data.id">
         <div class="m-item">
-            <div class="u-title">DEAD MAN</div>
-            <div class="u-author">练习生：djkjkm</div>
+            <div class="u-title" :class="{ 'marquee-active': isMarqueeActive }">
+                <div class="clip">
+                    <div class="marquee-wrapper" ref="marqueeWrapper" :class="{ 'marquee-animate': isMarqueeActive }">
+                        <span class="marquee-text" ref="marqueeText">
+                            {{ data.title }}
+                        </span>
+                        <span class="marquee-text copy">{{ data.title }}</span>
+                    </div>
+                </div>
+            </div>
+            <div class="u-author">
+                练习生：<a :href="`https://jx3box.com/user/${data.user_info.id}`" target="_blank">
+                    {{ data.user_info.display_name }}
+                </a>
+            </div>
             <div class="m-record">
-                <img class="u-needle" :src="`${imgRoot}web/item/needle.svg`" />
+                <img class="u-needle" :class="{ isPlaying }" :src="`${imgRoot}web/item/needle.svg`" />
                 <div class="u-record">
-                    <img class="u-avatar" src="https://cdn.jx3box.com/upload/avatar/2021/3/5/9393123.jpg?" />
+                    <img class="u-avatar" :src="data.user_info.avatar" />
                 </div>
             </div>
             <el-slider v-model="slider" size="small" :show-tooltip="false" />
             <div class="m-play">
                 <img class="u-icon" :src="`${imgRoot}web/item/left.svg`" />
-                <img class="u-icon u-play" :src="`${imgRoot}web/item/play.svg`" />
+                <img
+                    class="u-icon u-play"
+                    :src="`${imgRoot}web/item/${isPlaying ? 'stop.svg' : 'play.svg'}`"
+                    @click="togglePlay"
+                />
                 <img class="u-icon" :src="`${imgRoot}web/item/right.svg`" />
             </div>
             <div class="m-link">
                 <div class="u-like">
                     <img class="u-icon" :src="`${imgRoot}web/item/like.svg?jx3cxk`" />
-                    982391
+                    {{ data.amount }}
                 </div>
-                <a :href="``" target="_blank">查看原帖</a>
+                <a :href="`${link}${data.sub_title}`" target="_blank">查看原帖</a>
             </div>
         </div>
         <div class="m-button">
@@ -29,13 +46,60 @@
             <img class="u-icon u-hover" :src="`${imgRoot}web/item/emoji-2.svg`" />
             <img class="u-icon u-active" :src="`${imgRoot}web/item/like.svg?jx3cxk`" />
         </div>
+        <div class="m-bilibili" v-html="play"></div>
     </div>
 </template>
 <script>
+import { __cdn, __Root } from "@/utils/config";
 export default {
     inject: ["__imgRoot"],
+    props: {
+        data: {
+            type: Object,
+            default: () => {},
+        },
+    },
     data() {
-        return { imgRoot: this.__imgRoot, slider: 0 };
+        return {
+            imgRoot: this.__imgRoot,
+            link: __Root + "community/",
+
+            // 滚动标题
+            isMarqueeActive: false,
+            maxWidth: 300,
+            scrollSpeed: 40,
+            slider: 0,
+
+            // 播放
+            isPlaying: false,
+        };
+    },
+
+    methods: {
+        checkTextWidth() {
+            const textEl = this.$refs.marqueeText;
+            const wrapperEl = this.$refs.marqueeWrapper;
+            if (!textEl || !wrapperEl) return;
+
+            const singleTextWidth = textEl.offsetWidth;
+            this.isMarqueeActive = singleTextWidth > this.maxWidth;
+
+            if (this.isMarqueeActive) {
+                const duration = singleTextWidth / this.scrollSpeed;
+                wrapperEl.style.animationDuration = `${duration}s`;
+            } else {
+                wrapperEl.style.animationDuration = "";
+            }
+        },
+        togglePlay() {
+            this.play = this.play ? "" : this.data.content;
+            this.isPlaying = !this.isPlaying;
+        },
+    },
+    mounted() {
+        this.$nextTick(() => {
+            this.checkTextWidth();
+        });
     },
 };
 </script>
@@ -59,5 +123,72 @@ export default {
 }
 .el-slider__button {
     .size(12px);
+}
+</style>
+<style scoped>
+.marquee-wrapper {
+    white-space: nowrap;
+    position: relative;
+    display: inline-block;
+    transition: transform 0s linear;
+}
+
+.marquee-text {
+    display: inline-block;
+}
+.marquee-text::after {
+    content: "》";
+    display: inline-block;
+}
+.marquee-text::before {
+    content: "《";
+    display: inline-block;
+}
+.marquee-text.copy::after,
+.marquee-text.copy::before {
+    display: none;
+}
+.marquee-text.copy {
+    margin-left: 50px;
+    display: none;
+}
+.marquee-animate {
+    animation: marquee linear infinite;
+    animation-fill-mode: forwards;
+}
+.marquee-active .marquee-text.copy {
+    display: inline-block;
+}
+.marquee-animate .marquee-text::after,
+.marquee-animate .marquee-text::before {
+    display: none;
+}
+
+.marquee-active::after {
+    content: "》";
+    display: inline-block;
+    position: absolute;
+    right: -28px;
+    top: 50%;
+    transform: translateY(-50%);
+    z-index: 1;
+}
+.marquee-active::before {
+    content: "《";
+    display: inline-block;
+    position: absolute;
+    left: -28px;
+    top: 50%;
+    transform: translateY(-50%);
+    z-index: 1;
+}
+
+@keyframes marquee {
+    0% {
+        transform: translateX(0);
+    }
+    100% {
+        transform: translateX(-100%);
+    }
 }
 </style>
--- src/pages/event/jx3cxk/components/Vote.vue
+++ src/pages/event/jx3cxk/components/Vote.vue
@@ -1,17 +1,57 @@
 <template>
     <div class="m-content m-vote wp">
-        <vote-item></vote-item>
+        <template v-if="list.length">
+            <vote-item v-for="item in list" :key="item.id" :data="item" @toPlay="toPlay"></vote-item>
+        </template>
     </div>
 </template>
 <script>
 import voteItem from "./Item.vue";
-
+import User from "@jx3box/jx3box-common/js/user.js";
+import { shuffle } from "lodash";
+import { getProgramDetail, getMyVote } from "@/service/event/vote";
 export default {
     components: {
         voteItem,
     },
     data() {
-        return {};
+        return {
+            id: 28, // 投票ID
+            list: [],
+            play: "",
+        };
+    },
+    methods: {
+        loadData() {
+            this.loading = true;
+            getProgramDetail(this.id)
+                .then(async (res) => {
+                    const list = shuffle(res.data?.data?.vote_items || []);
+                    this.list = list.map((item) => {
+                        if (!item?.user_info?.avatar) item.user_info.avatar = `${__cdn}image/common/avatar.png`;
+                        item.slider = 0;
+                        return item;
+                    });
+                    if (User.isLogin()) await this.loadMyVote();
+                })
+                .finally(() => {
+                    this.loading = false;
+                });
+        },
+        async loadMyVote() {
+            const myVote = await getMyVote(this.id);
+            this.myVote = myVote.data?.data?.list || [];
+            this.list = this.list.map((item) => {
+                item.isVoted = this.myVote.some((e) => e.vote_item_id == item.id);
+                return item;
+            });
+        },
+        toPlay(data) {
+            this.play = data.content;
+        },
+    },
+    mounted() {
+        this.loadData();
     },
 };
 </script>
